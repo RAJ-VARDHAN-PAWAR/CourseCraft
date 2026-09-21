18c3525f11bb74a08c67eab511b3bb3274bc65b1
RAJ-VARDHAN-PAWAR <b.rajvardhanpawar@gmail.com>
2026-09-21 15:17:18 +0530

    Connected dataBase, and Rest methods

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 const express = require("express")
+const connectDB = require("./config/db")
+const dotenv = require("dotenv")
+const courseRoute = require("./routes/courseRoutes")
 const app = express()
+dotenv.config()
 
-app.get("/welcome",(req,res)=>{
-    res.send("Welcome, working....")
-})
-
+app.use("/api/courses",courseRoute)
+connectDB()
 app.listen(3000, ()=>{
     console.log("Listening to the PORT....")
 })
