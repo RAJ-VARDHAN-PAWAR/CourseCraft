5a6d63fec0a44dc0cc66e282412b06928494105e
RAJ-VARDHAN-PAWAR <b.rajvardhanpawar@gmail.com>
2026-09-22 15:07:38 +0530

    SignUp and Login done

--- server/routes/courseRoutes.js
+++ server/routes/courseRoutes.js
@@ -1,20 +1,12 @@
 const express = require("express")
+const { protect } = require("../middleware/authMiddleware")
+const { getCourse, createCourse, updateCourse, deleteCourse, getCourseById } = require("../controllers/courseController")
 const courseRoute = express.Router()
 
-courseRoute.get("/" , (req,res)=>{
-
-})
-courseRoute.post("/" , (req,res)=>{
-    
-})
-courseRoute.get("/:id" , (req,res)=>{
-    
-})
-courseRoute.put("/:id" , (req,res)=>{
-    
-})
-courseRoute.delete("/:id" , (req,res)=>{
-    
-})
+courseRoute.get("/" , getCourse)
+courseRoute.post("/" , createCourse)
+courseRoute.get("/:id" , getCourseById)
+courseRoute.put("/:id" , updateCourse)
+courseRoute.delete("/:id" , deleteCourse)
 
 module.exports = courseRoute
